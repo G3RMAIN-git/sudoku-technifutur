6b8671e5c2c2af5cf4369e2918a93228a518960c
Yannick <yannick.boogaerts@technifutur.be>
2022-05-03 15:17:39 +0200

    main du projet

---
+++ src/be/technifutur/sudoku/Main.java
@@ -0,0 +1,9 @@
+package be.technifutur.sudoku;
+
+import be.technifutur.sudoku.vue.SudokuVue3X3;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new SudokuVue3X3().getGrilleVide());
+    }
+}

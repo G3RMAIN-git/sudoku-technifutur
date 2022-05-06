ba33409175f2a721dfd2dc37a59d1acdfa945525
Yannick <yannick.boogaerts@technifutur.be>
2022-05-06 13:48:02 +0200

    test de test pour l'ajout de valeur via le controleur

---
+++ sudokuTest/src/be/technifutur/sudoku/controler/SudokuInitControlerTest.java
@@ -0,0 +1,40 @@
+package be.technifutur.sudoku.controler;
+
+import be.technifutur.sudoku.model.SudokuModel9x9;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SudokuInitControlerTest {
+
+    private SudokuInitControler controler;
+    private SudokuModel9x9 model;
+
+    @BeforeEach
+    void ini() {
+        controler = new SudokuInitControler();
+        model = new SudokuModel9x9();
+        controler.setSudoku(model);
+    }
+
+    @Test
+    @DisplayName("ajout d'un model au controler")
+    void testAddVueModel() {
+        Assertions.assertSame(model, controler.getSudoku());
+    }
+
+    @Test
+    @DisplayName("ajout valeur 3 en 1,2")
+    void testAddValue1inl1c2() {
+        controler.request("1.2.3");
+        Assertions.assertEquals('3', model.getValue(0, 1));
+    }
+
+    @Test
+    @DisplayName("ajout valeur 9 en 9,9")
+    void testAddValue9inl9c9() {
+        controler.request("9.9.9");
+        Assertions.assertEquals('9', model.getValue(8, 8));
+    }
+}

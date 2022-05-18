8e819726801c973e16f26f883008d2b298ee4684
Germain <germain.buch@gmail.com>
2022-05-18 10:01:48 +0200

    Texte 2X2 passé

---
+++ src/be/technifutur/sudoku/vue/SudokuVue2X2.java
@@ -0,0 +1,16 @@
+package be.technifutur.sudoku.vue;
+
+public class SudokuVue2X2 {
+    public String getGrilleVide() {
+        return """
+                +------+-----+
+                | . .  | . . |
+                | . .  | . . |
+                +------+-----+
+                | . .  | . . |
+                | . .  | . . |
+                +------+-----+
+                """;
+    }
+
+}

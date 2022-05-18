82e78a85de52feab8acc58c2f70217eea6dcdbfc
Germain <germain.buch@gmail.com>
2022-05-18 10:53:59 +0200

    test 2X2 isempty gecolumn et getline ok

---
+++ src/be/technifutur/sudoku/model/SudokuModel2X2.java
@@ -0,0 +1,15 @@
+package be.technifutur.sudoku.model;
+
+public class SudokuModel2X2 {
+    public int getLineSize() {
+        return 4;
+    }
+
+    public int getColumnSize() {
+        return 4;
+    }
+
+    public boolean isEmpty(int line, int column) {
+        return true;
+    }
+}

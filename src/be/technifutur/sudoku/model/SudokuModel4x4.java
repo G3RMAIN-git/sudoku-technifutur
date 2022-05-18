6b3c13ee7837d8b824b6c493e992b3fd59f2cbbe
Germain <germain.buch@gmail.com>
2022-05-18 16:09:37 +0200

    CA MARCHE COUSIN TOUCHE PAS A CA !!!! '4x4'

--- src/be/technifutur/sudoku/model/SudokuModel4x4.java
+++ src/be/technifutur/sudoku/model/SudokuModel4x4.java
@@ -1,6 +1,9 @@
 package be.technifutur.sudoku.model;
 
 public class SudokuModel4x4 {
+
+    public static final char EMPTY_VALUE = 0;
+    char[][] grille = new char[4][4];
     public int getLineSize() {
         return 4;
     }
@@ -11,5 +14,17 @@ public class SudokuModel4x4 {
 
     public boolean isEmpty(int line, int column) {
         return true;
+    }
+
+    public void setValue(int ligne, int colone, char valeur) {
+
+        //char[] tableau = new char[81];
+        grille[ligne][colone]=valeur;
+
+    }
+
+    public char getValue(int ligne, int colonne) {
+        char value = grille[ligne][colonne] ;
+        return value;
     }
 }
